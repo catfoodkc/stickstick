1ccd2c375f1203341d1232f024c06d776843832f
catfood <80357325+catfoodkc@users.noreply.github.com>
2021-04-06 18:26:30 -0400

    Update stick.py
    added felix's name

--- stick.py
+++ stick.py
@@ -5,7 +5,7 @@ import random
 import pyperclip
 
 stick = [
-"stick", "bad", "ophelia", "gun", "bad", "fds", "stick", "fds", "stick", "ophelia", "stick", "ophelia", "bad", "bad", "naughty", "fds", "stick", "whip", "bad", "meth", "stick", "evil"
+"stick", "bad", "ophelia", "gun", "bad", "fds", "stick", "fds", "stick", "ophelia", "stick", "ophelia", "bad", "bad", "naughty", "fds", "stick", "whip", "bad", "meth", "stick", "evil", "felix"
 ] #i probably couldve done something with a weighted choice but holy shit this is not the kind of program i put effort into
 #so instead i just broke down on a list
 porb = random.randint(10,50)
